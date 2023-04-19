d5d169cfe96d60f7097f5a96a7ba33a34b187275
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-19 16:33:12 +0600

    login page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,74 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <div className="flex justify-between navbar bg-primary text-primary-content">
-        <div>
-          <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
+    <div className="navbar bg-base-100 shadow-lg">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <NavLink className="" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="" to="/register">
+                Register
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <div>
-          {" "}
-          <NavLink className="btn btn-ghost normal-case text-xl" to="/">
-            Home
-          </NavLink>
-          <NavLink className="btn btn-ghost normal-case text-xl" to="/login">
-            Login
-          </NavLink>
-          <NavLink className="btn btn-ghost normal-case text-xl" to="/register">
-            Register
-          </NavLink>
+        <a className="btn btn-ghost normal-case text-2xl md:text-3xl font-semibold">
+          Auth Master
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-3">
+            <li>
+              <NavLink className="" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="" to="/register">
+                Register
+              </NavLink>
+            </li>
+          </ul>
         </div>
+        <Link to="/" className="btn">
+          Get started
+        </Link>
       </div>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,44 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-3xl md:text-4xl font-bold mb-5">Please Login!</h1>
+        </div>
+        <div className="card shadow-2xl bg-base-100 w-full md:w-96 ">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
